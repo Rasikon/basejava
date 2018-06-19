42893ab277cd8d69f953d5669a6e4a16736699a9
Rasikon <redreev.ivan@gmail.com>
2018-06-19 22:24:01 +0500

    Fix AbstractArrayStorage

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -59,7 +59,7 @@ public abstract class AbstractArrayStorage implements Storage {
     public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index < 0) {
-            System.out.println("Resume " + uuid + " not exist");
+            System.out.println("Resume with uuid =" + uuid + " not exist");
             return null;
         }
         return storage[index];
