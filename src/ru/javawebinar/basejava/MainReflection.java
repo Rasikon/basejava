3239892d1aa60d6a59d2dfe0499ca93676d295ff
Rasikon <redreev.ivan@gmail.com>
2018-06-22 08:27:32 +0500

    FixFieldResume

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -9,13 +9,13 @@ import java.lang.reflect.Method;
 public class MainReflection {
 
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Resume resume = new Resume();
+        Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
-        System.out.println(field.get(r));
-        field.set(r, "new_uuid");
-        Method method = r.getClass().getMethod("toString",null);
-        System.out.println(method.invoke(r,null));
+        System.out.println(field.get(resume));
+        field.set(resume, "new_uuid");
+        Method method = resume.getClass().getMethod("toString",null);
+        System.out.println(method.invoke(resume,null));
     }
 }
