47cfcb1d59af191e56853344b3e3dc381f072474
Rasikon <redreev.ivan@gmail.com>
2018-06-27 13:58:58 +0500

    FixTest

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -7,6 +7,7 @@ import ru.javawebinar.basejava.model.Resume;
 
 public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
 
+
     public AbstractArrayStorageTest(Storage storage) {
         super(storage);
     }
