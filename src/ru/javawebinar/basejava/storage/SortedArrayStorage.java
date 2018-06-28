146a73a4548218e8221ea702b746c7c361649026
Rasikon <redreev.ivan@gmail.com>
2018-06-28 16:25:52 +0500

    Rename int

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -7,17 +7,17 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void remove(int index) {
-        int number = size - index - 1;
-        if (number > 0) {
-            System.arraycopy(storage, index + 1, storage, index, number);
+        int shift = size - index - 1;
+        if (shift > 0) {
+            System.arraycopy(storage, index + 1, storage, index, shift);
         }
     }
 
     @Override
     protected void paste(Resume resume, int index) {
-        int number = -index - 1;
-        System.arraycopy(storage, number, storage, number + 1, size - number);
-        storage[number] = resume;
+        int shift = -index - 1;
+        System.arraycopy(storage, shift, storage, shift + 1, size - shift);
+        storage[shift] = resume;
     }
 
     @Override
