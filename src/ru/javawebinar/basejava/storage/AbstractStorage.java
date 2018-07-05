d910d55cab42f8f867827f70e3b4fc29eb95f0d6
Rasikon <redreev.ivan@gmail.com>
2018-07-05 13:10:10 +0500

    add logging

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,25 +6,32 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<K> implements Storage {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         K key = getIfNotExist(resume.getUuid());
         doUpdate(key, resume);
     }
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         K key = getIfExist(resume.getUuid());
         doSave(key, resume);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         K key = getIfNotExist(uuid);
         doDelete(key);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         K key = getIfNotExist(uuid);
         return doGet(key);
     }
@@ -32,6 +39,7 @@ public abstract class AbstractStorage<K> implements Storage {
     private K getIfExist(String uuid) {
         K key = getKey(uuid);
         if (isExist(key)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         } else {
             return key;
@@ -41,6 +49,7 @@ public abstract class AbstractStorage<K> implements Storage {
     private K getIfNotExist(String uuid) {
         K key = getKey(uuid);
         if (!isExist(key)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         } else {
             return key;
@@ -49,6 +58,7 @@ public abstract class AbstractStorage<K> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = getCopy();
         Collections.sort(list);
         return list;
