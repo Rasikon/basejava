1e4456b4fe12a979ff09bb1930fdd8043b13a5b8
Rasikon <redreev.ivan@gmail.com>
2018-08-14 10:57:27 +0500

    Fix Test Thread

--- src/ru/javawebinar/basejava/TestThread.java
+++ src/ru/javawebinar/basejava/TestThread.java
@@ -1,42 +1,38 @@
 package ru.javawebinar.basejava;
 
 public class TestThread {
-    public static Object Lock1 = new Object();
-    public static Object Lock2 = new Object();
+    public static String Lock1 = "lock1";
+    public static String Lock2 = "lock2";
 
     public static void main(String args[]) {
-        ThreadDemo1 T1 = new ThreadDemo1();
-        ThreadDemo2 T2 = new ThreadDemo2();
+        ThreadDemo1 T1 = new ThreadDemo1(Lock1, Lock2);
+        ThreadDemo1 T2 = new ThreadDemo1(Lock2, Lock1);
         T1.start();
         T2.start();
     }
 
     private static class ThreadDemo1 extends Thread {
-        public void run() {
-            synchronized (Lock1) {
-                System.out.println("Thread 1: Holding lock 1...");
+        Object lock1;
+        Object lock2;
 
-                try { Thread.sleep(10); }
-                catch (InterruptedException e) {}
-                System.out.println("Thread 1: Waiting for lock 2...");
-
-                synchronized (Lock2) {
-                    System.out.println("Thread 1: Holding lock 1 & 2...");
-                }
-            }
+        public ThreadDemo1(Object lock1, Object lock2) {
+            this.lock1 = lock1;
+            this.lock2 = lock2;
         }
-    }
-    private static class ThreadDemo2 extends Thread {
+
         public void run() {
-            synchronized (Lock2) {
-                System.out.println("Thread 2: Holding lock 2...");
+            System.out.println("Waiting "+lock1);
+            synchronized (lock1) {
+                System.out.println("Holding "+lock1);
 
-                try { Thread.sleep(10); }
-                catch (InterruptedException e) {}
-                System.out.println("Thread 2: Waiting for lock 1...");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+                System.out.println("Waiting "+ lock2);
 
-                synchronized (Lock1) {
-                    System.out.println("Thread 2: Holding lock 1 & 2...");
+                synchronized (lock2) {
+                    System.out.println("Holding "+lock2);
                 }
             }
         }
